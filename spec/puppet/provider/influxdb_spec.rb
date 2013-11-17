48d9c9d667e0c392bc6ece77ed489a001aeffe34
Ørjan Blom <blom@blom.tv>
2013-11-17 23:46:43 +0100

    Add spec

--- spec/puppet/provider/influxdb_spec.rb
+++ spec/puppet/provider/influxdb_spec.rb
@@ -1,9 +1,15 @@
 require "spec_helper"
 
 describe Puppet::Provider::InfluxDB do
+  let(:influxdb) { described_class.new }
+  before { influxdb.stub(:resource).and_return({}) }
+
+  describe "#influxdb" do
+    it { influxdb.influxdb.should be_a InfluxDB::Client }
+  end
+
   describe "#config" do
     it "returns a hash with keys converted to symbols" do
-      influxdb = described_class.new
       influxdb.stub(:resource).and_return "config" => { "foo" => 0 }
       influxdb.send(:config).should eql :foo => 0
     end
